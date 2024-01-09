288af3b6391a223c3b2121c9cf147c98088c964f
Hamza Chenguiti <chenguitiha@gmail.com>
2024-01-09 11:49:05 +0100

    add TailwindCSS custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'weather-primary': '#00668A',
+        'weather-secondary': '#004E71'
+      },
+      fontFamily: {
+        Roboto: ['Roboto, sans-serif']
+      },
+      container: {
+        center: true,
+        padding: '2rem'
+      },
+      screens: {
+        sm: '640px',
+        md: '768px'
+      }
+    }
   },
   plugins: []
 }
